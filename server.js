683bfee9e9773d97cd8e4eedb71d593a847fe747
luisgcodes <lcienfuegos95@gmail.com>
2022-06-24 14:05:13 -0700

    gitignore

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Formats data to json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require('./routes'))
 
 // Initialize server
 const app = express();
+
+// PORT
 const PORT = process.env.PORT || 3001;
